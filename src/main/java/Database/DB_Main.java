ad5c0a544456ce8518b33414f3ca968f639ba88d
Romaine Murray <romaine@pop-os.localdomain>
2020-04-19 10:11:47 -0500

    Added DB

--- src/main/java/Database/DB_Main.java
+++ src/main/java/Database/DB_Main.java
@@ -1,25 +1,34 @@
 package Database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DB_Main {
-    private Connection con;
-    private Statement st;
-    
+    private static Connection con;
+    private static Statement st;
+    private static ResultSet rs;
+
     public static void main(String[] args) {
+        DB_Main main = new DB_Main();
     }
 
     public DB_Main() {
-    }
-
-    public static Connection getConnection() throws Exception {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/SLEMS", "root", "root");
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * from SLEMS");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) +
+                        " " + rs.getString(2) +
+                        " " + rs.getString(3));
+                con.close();
+            }
+
         } catch (Exception var1) {
             var1.printStackTrace();
         }
-
-        return null;
     }
 }
